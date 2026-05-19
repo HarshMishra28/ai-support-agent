f01915f23492dc5cc0a4476f7b0ffc6b668ad64e
Sundaram Mishra <sundarammishra2000@gamil.com>
2026-05-19 17:13:57 +0530

    Phase 1 complete - Spring Boot Prject setup with MySQL config

---
+++ Desktop/ai-support-agent/src/test/java/com/sundaram/aisupportagent/AiSupportAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundaram.aisupportagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSupportAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
